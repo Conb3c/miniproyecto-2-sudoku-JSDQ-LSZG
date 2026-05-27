0458bfeb480c66c759f31c04c94f80b291a9e37c
Conb3c <juseduqu@gmail.com>
2026-05-27 15:13:30 -0500

    Generate sudoku, validate Sudoku

--- src/main/java/Controller/GameController.java
+++ src/main/java/Controller/GameController.java
@@ -1,11 +1,28 @@
 package Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.input.KeyEvent;
 
 public class GameController {
 
     @FXML
     public void initialize() {
 
+    }
+
+    public void onNumberClicked(ActionEvent actionEvent) {
+    }
+
+    public void onNewGameClicked(ActionEvent actionEvent) {
+    }
+
+    public void onHelpClicked(ActionEvent actionEvent) {
+    }
+
+    public void onResetClicked(ActionEvent actionEvent) {
+    }
+
+    public void onCellKeyReleased(KeyEvent keyEvent) {
     }
 }
