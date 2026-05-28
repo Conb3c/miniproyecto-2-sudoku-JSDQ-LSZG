ddd85f6095cc6974f4b1a8f81dd5081499116e46
Lesly Zapata <lesly.zapata@correounivalle.edu.co>
2026-05-28 10:24:39 -0500

    Fix FXML loading, board initialization, and validation issues

--- src/main/java/Model/Main.java
+++ src/main/java/Model/Main.java
@@ -1,7 +1,11 @@
 package Model;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -10,6 +14,16 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource(
+                       "/com/example/miniproyecto2sudokujsdqlszg/GameView.fxml"
+                )
+        );
+        Scene scene = new Scene(loader.load());
+        primaryStage.setTitle("Sudoku");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
